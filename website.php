3d9e07918de3ab8d300e8d6942f21b3959056eef
misoi99 <hanningtonchumo99@gmail.com>
2019-11-01 14:12:12 +0300

    website created

---
+++ website.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>HANNINGTON CHUMO</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <style>
+  .fakeimg {
+    height: 200px;
+    background: #aaa;
+  }
+  .heartBeat {
+  -webkit-animation-name: heartBeat;
+  animation-name: heartBeat;
+  -webkit-animation-duration: 1.3s;
+  animation-duration: 1.3s;
+  -webkit-animation-timing-function: ease-in-out;
+  animation-timing-function: ease-in-out;
+}
+  </style>
+</head>
+<body>
+
+<div class="support-bar bg-primary">
+        <div class="bg-success"></div>
+        <img src="images/profile.png" width="300px" height="300px">
+<div class="support-middle"><!-- support middle start -->
+                        <marquee width="60%" direction="right" height="100px">
+                               call 0726575778 /
+                                info@hannington2019gmail.com
+                                </marquee>
+</div>
+<nav class="navbar navbar-expand-sm bg-success navbar-dark">
+  <a class="navbar-brand" href="#">HOME</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link heartBeat" href="#about">ABOUT</a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link heartBeat" href="#services">SERVICES</a>
+        <li class="nav-item">
+        <a class="nav-link heartBeat" href="#contact">CONTACT</a>
+      </li>
+      </li>    
+    </ul>
+  </div>  
+</nav>
+<div>
+
+<nav class="navbar navbar-expand-sm bg-success navbar-dark">
+<form action="login.php" method="post">
+<input type="text" name="mailuid" placeholder="username/email"><br>
+<input type="password" name="pwd" placeholder="password"><br>
+<button class ="btn btn-primary" type="submit" name="login-submit">Submit</button>
+</form>
+</form>
+</div>
+
+
+<!-- About Section -->
+<section class="bg-primary text-white mb-0">
+
+    <div class="item">
+    <nav class="navbar navbar-expand-sm bg-success navbar-dark">
+    <img src="images/QET.png" width="400px" height="300px">
+    <h1>SOFTWARE ENGINEERING COMPANY</h1><br>
+    <img src="images/QET PROFILE.jpg" width="600px" height="400px">
+    </div>
+    <div class="container" id="about">
+      <h2 class="text-center text-uppercase text-white">About</h2>
+      <hr class="star-light mb-5">
+      <div class="row">
+        <div class="col-lg-4 ml-auto">
+          <p class="lead">Customizing systems,developing systems that reduce paper work and increase efficiency,installation and configuration of systems such as Q-Afya,Creating websites,Networking computers in organizations and providing other IT support services</p>
+        </div>
+        <div class="col-lg-4 mr-auto">
+          <p class="lead">Our services are timely and convinient and we offer after sales services and monitoring of sytsems</p>
+        </div>
+      </div>
+
+<div class="container bg-danger" id= "services" style="margin-top:30px">
+  <div class="row">
+  <h2>Our services</h2><br>
+      <p>Our experienced team of computer engineers,software engineers and computer scientists provide solutions that enable companies from around the world to improve their operation processes as well as increase product quality and productivity.</p>
+      <br>
+    <div class="col-sm-4">
+      <h2>Hospital Management Information System(HRMIS)</h2><h1>Q-AFYA</h2>
+      <img src="images/Q-AFYA.png" width="300px" height="250px">
+      <p> Q SACCO</P>
+      <img src="images/QET BANK.jpg" width="300px" height="250px">
+      <p>Sacco core banking system.</p>
+      <h3>Q-PHARMACY MANAGER</h3>
+      <img src="images/pharmacy 1.jpg" width="300px" height="250px">
+      <hr class="d-sm-none">
+      <h2>POINT OF SALE</h2>
+      <img src="images/Point of Sale.png" width="400px" height="350px">
+      <p> Q-biz pos</p>
+      <img src="images/Q-BIZ.jpg" width="400px" height="300px">
+    </div>
+    <div class="col-sm-8"id="services">
+      
+      <h2>WEB DESIGNERS</h2><br>
+      <h5>we develop websites that ignite your business</h5><br>
+      <img src="images/website.png" width="300px" height="250px">
+      <img src="images/website 2.png" width="300px" height="250px">
+      <h6> ANDROID DEVELOPERS</h6>
+    <img src="images/android 2.png" width="300px" height="250px">
+    <img src="images/android 3.png" width="300px" height="250px"><br>
+    <img src="images/android 4.png" width="300px" height="250px">
+   
+    </div>
+  </div>
+</div>
+
+      
+      
+<div class="bg-dark text-center" style="margin-bottom:10;" id="contact">
+<!-- Footer -->
+<footer class="footer text-center">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 mb-5 mb-lg-0">
+          
+      
+        </div>
+        <div class="col-md-4 mb-5 mb-lg-0">
+          <h4 class="text-uppercase mb-4">FOLLOW US ON SOCIAL MEDIA</h4>
+          <img src="images/whatsapp.png" width="50px" height="50px">
+    <img src="images/facebook.png" width="50px" height="50px">
+    <img src="images/twitter.png" width="50px" height="50px">   
+<p>contact us on +254726575778</P><br>
+
+  &copy hannington
+</div>
+</body>
+</html>
